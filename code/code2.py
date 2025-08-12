7284220733d33297d93a67db69de078bc5a5376a
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-08-12 22:58:59 +1000

    code
    i did some code

--- code/code2.py
+++ code/code2.py
@@ -3,7 +3,15 @@
 import pandas as pd
 
 def print_csv():
-    df = pd.read_csv('code/data.csv')
+    df = pd.read_csv(
+         'code/data.csv', 
+         on_bad_lines='skip',
+         header = None,
+         names = ["Timestamp",
+                "Do you play any gacha/lootbox games?",
+                "If yes, have you spent money on systems where the desired ""drop"" is not guaranteed?",
+                "How do you feel about spending money on lootbox components in games?"]
+         )
     df.drop(columns=['Timestamp'], inplace=True)
     pd.set_option('display.max_rows', 1000)
     print(df)
@@ -17,7 +25,7 @@ def main_menu():
         print("3. Search or filter data")
         print("4. Exit")
         
-        choice = int(input("Select an option (1-6): "))
+        choice = int(input("Select an option (1-4): "))
         if choice == 1:
                 print_csv()
 
