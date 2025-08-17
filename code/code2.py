1e89d9212d6458722141950d4b5124fcc145b78b
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-08-17 16:47:59 +1000

    Update code2.py
    coded more of option1 and option 3 and fixed things

--- code/code2.py
+++ code/code2.py
@@ -1,44 +1,85 @@
 # data_module.py
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import time
 
 def print_csv():
     df = pd.read_csv(
-         'code/data.csv', 
+         'code/data.csv',
          on_bad_lines='skip',
          header = None,
          names = ["Timestamp",
-                "Do you play any gacha/lootbox games?",
-                'If yes, have you spent money on systems where the desired ""drop"" is not guaranteed?',
-                "How do you feel about spending money on lootbox components in games?"]
+                "| Play lootbox games |",
+                " Spent money on lootbox systems |",
+                "Spending feelings (1 bad, 5 great) |"]
          )
     df.drop(columns=['Timestamp'], inplace=True)
+    df.dropna(inplace = True)
     pd.set_option('display.max_rows', 1000)
     print(df)
+
+def data_filtering ():
+        time.sleep(1)
+        print("Search for data:")
+
+
+        time.sleep(1)
+        data_filter_choice = int(input("Select an option (1-3): "))
 
 
 def main_menu():
     while True:
-        print("Data Viewer Interface")
-        print("1. View dataset")
-        print("2. View visualisation")
-        print("3. Search or filter data")
-        print("4. Exit")
-        
-        choice = int(input("Select an option (1-4): "))
+        time.sleep (1)
+        print("""\n
+ -------------------------
+|Data Viewer Interface:   |
+|                         |
+|1. View dataset          |
+|2. View visualisation    |
+|3. Search or filter data |
+|4. Exit                  | 
+ -------------------------              
+""")
+        choice = int(input("""
+----------------------
+Select an option (1-4): """))
         if choice == 1:
+                time.sleep (1)
+                print("\nWhether students at Gosford High School:\n")
                 print_csv()
         elif choice == 2:
                 print("yay")
         elif choice == 3:
-                print("yay!")
+                data_filtering()
         elif choice == 4:
-                print("Exiting program.")
+                print("\nExiting program!!\n")
+                break
+        elif choice == 32:
+                print("""
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⡤⠤⠤⠤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠉⠛⢦⣤⠶⠦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⠞⢋⡽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠀⠀⠙⢶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣰⠟⠁⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡀⠀⠀⠉⠓⠦⣤⣤⣤⣤⣤⣤⣄⣀⠀⠀⠀
+⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣷⡄⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣆⠀
+⠀⠀⣠⠞⠁⠀⠀⣀⣠⣏⡀⠀⢠⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠿⡃⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡆
+⢀⡞⠁⠀⣠⠶⠛⠉⠉⠉⠙⢦⡸⣿⡿⠀⠀⠀⡄⢀⣀⣀⡶⠀⠀⠀⢀⡄⣀⠀⣢⠟⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠃
+⡞⠀⠀⠸⠁⠀⠀⠀⠀⠀⠀⠀⢳⢀⣠⠀⠀⠀⠉⠉⠀⠀⣀⠀⠀⠀⢀⣠⡴⠞⠁⠀⠀⠈⠓⠦⣄⣀⠀⠀⠀⠀⣀⣤⠞⠁⠀
+⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠁⠀⢀⣀⣀⡴⠋⢻⡉⠙⠾⡟⢿⣅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠛⠉⠉⠀⠀⠀⠀
+⠘⣦⡀⠀⠀⠀⠀⠀⠀⣀⣤⠞⢉⣹⣯⣍⣿⠉⠟⠀⠀⣸⠳⣄⡀⠀⠀⠙⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠈⠙⠒⠒⠒⠒⠚⠋⠁⠀⡴⠋⢀⡀⢠⡇⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⢀⡾⠋⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⢸⡀⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇⠀⠀⠉⠋⠻⣄⠀⠀⠀⠀⠀⣀⣠⣴⠞⠋⠳⠶⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⠦⢤⠤⠶⠋⠙⠳⣆⣀⣈⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+bybye LOL \n """)
                 break
         else:
                 print("Invalid selection. Please choose a number between 1 and 4.")
-
-
+        
+print("\nLoading...")
 main_menu()
+
 
 
