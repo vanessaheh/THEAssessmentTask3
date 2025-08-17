77a1c1ef9a0feea9c767ed678ba2787694000398
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-08-17 21:38:41 +1000

    updated

--- code/code2.py
+++ code/code2.py
@@ -1,31 +1,63 @@
-# data_module.py
+#data_module.py
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
-def print_csv():
+def data():
     df = pd.read_csv(
          'code/data.csv',
          on_bad_lines='skip',
          header = None,
          names = ["Timestamp",
                 "| Play lootbox games |",
-                " Spent money on lootbox systems |",
-                "Spending feelings (1 bad, 5 great) |"]
+                "| Spent money on lootbox systems |",
+                "| Spending feelings (1-bad, 5-great) |"]
          )
     df.drop(columns=['Timestamp'], inplace=True)
     df.dropna(inplace = True)
     pd.set_option('display.max_rows', 1000)
-    print(df)
+    return df
+
+def print_csv():
+        df = data()
+        print(df)
 
 def data_filtering ():
-        time.sleep(1)
-        print("Search for data:")
+        while True:
+                time.sleep(1)
+                filter_choice = int(input("""
+\nFilter from:
+1. Whether students play lootbox games 
+2. Whether students spent money on lootbox games 
+3. How students feel about spending money on lootbox systems (1-bad, 5-great) 
+4. Exit 
+Select an option (1-4): """)) 
+        
+                if filter_choice == 1:
+                        df = data()
+                        time.sleep (1) 
+                        print("\n") 
+                        print(df.loc[:,["| Play lootbox games |"]]) 
+                
+                elif filter_choice == 2: 
+                        df = data() 
+                        time.sleep (1) 
+                        print("\n") 
+                        print(df.loc[:,["| Spent money on lootbox systems |"]]) 
+                
+                elif filter_choice == 3: 
+                        df = data()
+                        time.sleep (1) 
+                        print("\n") 
+                        print(df.loc[:,["| Spending feelings (1-bad, 5-great) |"]]) 
+                
+                elif filter_choice == 4: 
+                        main_menu() 
+                
+                else: 
+                        print("\nInvalid selection. Please choose a number between 1 and 4.")
 
-
-        time.sleep(1)
-        data_filter_choice = int(input("Select an option (1-3): "))
 
 
 def main_menu():
@@ -44,6 +76,7 @@ def main_menu():
         choice = int(input("""
 ----------------------
 Select an option (1-4): """))
+
         if choice == 1:
                 time.sleep (1)
                 print("\nWhether students at Gosford High School:\n")
