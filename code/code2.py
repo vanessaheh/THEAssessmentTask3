cd539b2641d587e3d5efaca2aa7638877a6a742b
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-08-18 12:29:34 +1000

    i fixed my yes,no
    i fixed it

--- code/code2.py
+++ code/code2.py
@@ -4,58 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
-def data():
-    df = pd.read_csv(
-         'code/data.csv',
-         on_bad_lines='skip',
-         header = None,
-         names = ["Timestamp",
-                "| Play lootbox games |",
-                "| Spent money on lootbox systems |",
-                "| Spending feelings (1-bad, 5-great) |"]
-         )
-    df.drop(columns=['Timestamp'], inplace=True)
-    df.dropna(inplace = True)
-    pd.set_option('display.max_rows', 1000)
-    return df
-
-def print_csv():
-        df = data()
-        print(df)
-
-def data_filtering ():
-        df = data()
-
-        while True:
-                time.sleep(1)
-                filter_choice = int(input("""
-\nFilter from:
-1. Whether students play lootbox games 
-2. Whether students spent money on lootbox games 
-3. How students feel about spending money on lootbox systems (1-bad, 5-great) 
-4. Exit 
-Select an option (1-4): """)) 
-        
-                if filter_choice == 1:
-                        time.sleep (1) 
-                        print("\n", df.loc[:,["| Play lootbox games |"]])
-                        values = df.loc[:,["| Play lootbox games |"]]
-
-                elif filter_choice == 2: 
-                        time.sleep (1) 
-                        print("\n", df.loc[:,["| Spent money on lootbox systems |"]]) 
-
-                elif filter_choice == 3: 
-                        time.sleep (1) 
-                        print("\n", df.loc[:,["| Spending feelings (1-bad, 5-great) |"]]) 
-                
-                elif filter_choice == 4: 
-                        main_menu() 
-                
-                else: 
-                        print("\nInvalid selection. Please choose a number between 1 and 4.")
-
-
+from data_module import data_filtering, view_dataset
 
 def main_menu():
     while True:
@@ -66,7 +15,7 @@ def main_menu():
 |                         |
 |1. View dataset          |
 |2. View visualisation    |
-|3. Filter data |
+|3. Filter data           |
 |4. Exit                  | 
  -------------------------              
 """)
@@ -77,7 +26,7 @@ Select an option (1-4): """))
         if choice == 1:
                 time.sleep (1)
                 print("\nWhether students at Gosford High School:\n")
-                print_csv()
+                view_dataset()
         elif choice == 2:
                 print("yay")
         elif choice == 3:
@@ -85,26 +34,6 @@ Select an option (1-4): """))
         elif choice == 4:
                 time.sleep (1)
                 print("\nExiting program!!\n")
-                break
-        elif choice == 32:
-                print("""
-
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⡤⠤⠤⠤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠉⠛⢦⣤⠶⠦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢀⣴⠞⢋⡽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠀⠀⠙⢶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣰⠟⠁⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡀⠀⠀⠉⠓⠦⣤⣤⣤⣤⣤⣤⣄⣀⠀⠀⠀
-⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣷⡄⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣆⠀
-⠀⠀⣠⠞⠁⠀⠀⣀⣠⣏⡀⠀⢠⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠿⡃⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡆
-⢀⡞⠁⠀⣠⠶⠛⠉⠉⠉⠙⢦⡸⣿⡿⠀⠀⠀⡄⢀⣀⣀⡶⠀⠀⠀⢀⡄⣀⠀⣢⠟⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠃
-⡞⠀⠀⠸⠁⠀⠀⠀⠀⠀⠀⠀⢳⢀⣠⠀⠀⠀⠉⠉⠀⠀⣀⠀⠀⠀⢀⣠⡴⠞⠁⠀⠀⠈⠓⠦⣄⣀⠀⠀⠀⠀⣀⣤⠞⠁⠀
-⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠁⠀⢀⣀⣀⡴⠋⢻⡉⠙⠾⡟⢿⣅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠛⠉⠉⠀⠀⠀⠀
-⠘⣦⡀⠀⠀⠀⠀⠀⠀⣀⣤⠞⢉⣹⣯⣍⣿⠉⠟⠀⠀⣸⠳⣄⡀⠀⠀⠙⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠈⠙⠒⠒⠒⠒⠚⠋⠁⠀⡴⠋⢀⡀⢠⡇⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⢀⡾⠋⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⢸⡀⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇⠀⠀⠉⠋⠻⣄⠀⠀⠀⠀⠀⣀⣠⣴⠞⠋⠳⠶⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⠦⢤⠤⠶⠋⠙⠳⣆⣀⣈⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-bybye LOL \n """)
                 break
         else:
                 print("Invalid selection. Please choose a number between 1 and 4.")
--- code/data_module.py
+++ code/data_module.py
@@ -1,11 +1,63 @@
 # data_module.py
 import pandas as pd
+import time
+def data_updated():
+    df = pd.read_csv(
+         'code/data.csv',
+         on_bad_lines='skip',
+         header = None,
+         names = ["Timestamp",
+                "| Play lootbox games |",
+                "| Spent money on lootbox systems |",
+                "| Spending feelings (1-bad, 5-great) |"]
+         )
+    df.drop(columns=['Timestamp'], inplace=True)
+    df.dropna(inplace = True)
+    substring = 'yes, no'
+    filter = df['| Play lootbox games |'].str.contains(substring)
+    df = df[~filter]
+    pd.set_option('display.max_rows', 1000)
+    return df
 
-def display_dataset_preview():
-    # Use pandas here
-    pass
+def view_dataset():
+        df = data_updated()
+        print(df)
 
-def update_data_entry():
-    # Use pandas here
-    pass
+def data_filtering ():
+        df = data_updated()
 
+        while True:
+                time.sleep(1)
+                filter_choice = int(input("""
+\nFilter from:
+1. Whether students play lootbox games 
+2. Whether students spent money on lootbox games 
+3. How students feel about spending money on lootbox systems (1-bad, 5-great) 
+4. Exit 
+Select an option (1-4): """)) 
+        
+                if filter_choice == 1:
+                        time.sleep (1) 
+                        print("\n", df.loc[:,["| Play lootbox games |"]])
+                        values = df.loc[:,["| Play lootbox games |"]]
+
+                elif filter_choice == 2: 
+                        time.sleep (1) 
+                        print("\n", df.loc[:,["| Spent money on lootbox systems |"]]) 
+
+                elif filter_choice == 3: 
+                        time.sleep (1) 
+                        print("\n", df.loc[:,["| Spending feelings (1-bad, 5-great) |"]]) 
+                
+                elif filter_choice == 4: 
+                        return 
+                
+                else: 
+                        print("\nInvalid selection. Please choose a number between 1 and 4.")
+
+
+
+        
+
+#error_rows = df[df['| Play lootbox games |'] != 'yes' or ['| Play lootbox games |'] != 'no'].index()
+    #df.drop(error_rows)
