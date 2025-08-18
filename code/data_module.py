7cac0536f5b882c13089ef7c08f0f0cf544055cd
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-08-18 22:28:09 +1000

    Update data_module.py
    the visaulisations

--- code/data_module.py
+++ code/data_module.py
@@ -1,6 +1,7 @@
 # data_module.py
 import pandas as pd
 import time
+import matplotlib.pyplot as plt
 
 def data_updated():
     df = pd.read_csv(
@@ -39,18 +40,19 @@ Select an option (1-4): """))
         
                 if filter_choice == 1:
                         time.sleep (1) 
-                        print("\n" + df.loc[:,["| Play lootbox games |"]])
+                        print(df.loc[:,["| Play lootbox games |"]])
                         values = df.loc[:,["| Play lootbox games |"]]
+                        
                         print(df["| Play lootbox games |"].value_counts())
 
                 elif filter_choice == 2: 
                         time.sleep (1) 
-                        print("\n"+ df.loc[:,["| Spent money on lootbox systems |"]])
+                        print(df.loc[:,["| Spent money on lootbox systems |"]])
                         print(df["| Spent money on lootbox systems |"].value_counts()) 
 
                 elif filter_choice == 3: 
                         time.sleep (1) 
-                        print("\n"+ df.loc[:,["| Spending feelings (1-bad, 5-great) |"]]) 
+                        print(df.loc[:,["| Spending feelings (1-bad, 5-great) |"]]) 
                         print(df["| Spending feelings (1-bad, 5-great) |"].value_counts())
 
                 elif filter_choice == 4: 
@@ -58,3 +60,42 @@ Select an option (1-4): """))
                 
                 else: 
                         print("\nInvalid selection. Please choose a number between 1 and 4.")
+
+def data_visualisation():
+    while True:
+        time.sleep(1)
+        visualisation_choice = int(input("""
+
+View visualisation of: 
+1. Gacha game players vs spenders
+2. Comparison of spending feelings (1-bad, 5-great)
+3. Exit
+"""))
+
+        if visualisation_choice == 1:
+                plt.clf()
+                categories = ['gacha game players', 'gacha game spenders ']
+                values = [73/153 * 100, 53/153 * 100]
+
+                plt.bar(categories, values, color= ['skyblue', 'orange'])
+                plt.title('Gacha game players vs spenders')
+                plt.ylabel('percentage of total students (%)')
+
+                plt.show(block= False)
+
+        elif visualisation_choice == 2: 
+                plt.clf()
+                categories = ['1', '2', '3', '4', '5']
+                values = [70, 36, 16, 8, 25]
+
+                plt.bar(categories, values, color= ['skyblue', 'orange', 'yellow', 'blue', '#FC6238'])
+                plt.title('Comparison of spending feelings (1-bad, 5-great)')
+                plt.ylabel('Number of students chosen option')
+
+                plt.show(block= False)
+
+        elif visualisation_choice == 3: 
+                return 
+                
+        else: 
+                print("\nInvalid selection. Please choose a number between 1 and 4.")
