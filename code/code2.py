6285590d9d3aa122251fec0d8dad3fd5a01cf273
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-08-18 22:58:02 +1000

    Update code2.py

--- code/code2.py
+++ code/code2.py
@@ -1,14 +1,19 @@
 #data_module.py
 
+#imported libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
+#Importing my functions for data_module.py
 from data_module import data_filtering, view_dataset, data_visualisation
 
+#main menu as a function
 def main_menu():
-    while True:
-        time.sleep (1)
+    while True: #plays this as the main menu until the user chooses to exit
+        time.sleep (1) # Pause for one second before showing menu
+        
+        #user interface
         print("""\n
  -------------------------
 |Data Viewer Interface:   |
@@ -19,6 +24,7 @@ def main_menu():
 |4. Exit                  | 
  -------------------------              
 """)
+        #the number they choose goes through an if elif else statement to display different outcomes
         choice = int(input("""
 ----------------------
 Select an option (1-4): """))
@@ -26,7 +32,7 @@ Select an option (1-4): """))
         if choice == 1:
                 time.sleep (1)
                 print("\nWhether students at Gosford High School:\n")
-                view_dataset()
+                view_dataset() #my functions
         elif choice == 2:
                 data_visualisation()
         elif choice == 3:
@@ -34,10 +40,11 @@ Select an option (1-4): """))
         elif choice == 4:
                 time.sleep (1)
                 print("\nExiting program!!\n")
-                break
-        else:
+                break #exit the loop and end program
+        else: #handle invalid input
                 print("Invalid selection. Please choose a number between 1 and 4.")
         
+#loading message and launch the main menu
 print("\nLoading...")
 main_menu()
 
